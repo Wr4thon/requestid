28b684d301bd29e67bec0d53c66cb5af600b644b
Wr4thon <jokolata@live.de>
2020-11-05 19:00:15 +0100

    fixed echoMiddleware description

--- echo.go
+++ echo.go
@@ -2,7 +2,7 @@ package requestid
 
 import "github.com/labstack/echo/v4"
 
-// EchoMiddleware Sets the requestID
+// EchoMiddleware puts the requestID into the echo context.
 func EchoMiddleware() echo.MiddlewareFunc {
 	return func(handlerFunc echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
